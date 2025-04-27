29fa64dbaf2b8668891affeceafc507a06dba1b9
Jv-Torelli <joao.vmatos@sptech.school>
2025-04-27 11:09:53 -0300

    Banco de dados, modelagem e script

---
+++ Banco de Dados/script CarConnect.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE carConnect;
+USE carConnect;
+
+CREATE TABLE usuario (
+    idusuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    dtNascimento DATE,
+    bio VARCHAR(200),
+    fotoPerfil VARCHAR(500),
+    dtCriacaoConta DATE
+);
+
+CREATE TABLE postagem (
+    idpostagem INT PRIMARY KEY AUTO_INCREMENT,
+    conteudo VARCHAR(200),
+    dtPostagem DATE,
+    fkUsuario INT,
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idusuario)
+);
+
+CREATE TABLE postagemFotos (
+    idpostagemFotos INT PRIMARY KEY AUTO_INCREMENT,
+    urlFoto VARCHAR(300),
+    fkPostagem INT,
+    FOREIGN KEY (fkPostagem) REFERENCES postagem(idpostagem)
+);
+
+CREATE TABLE curtida (
+    idcurtida INT PRIMARY KEY AUTO_INCREMENT,
+    fkPostagem INT,
+    fkUsuario INT,
+    FOREIGN KEY (fkPostagem) REFERENCES postagem(idpostagem),
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idusuario)
+);
+
+CREATE TABLE comentario (
+    idcomentario INT PRIMARY KEY AUTO_INCREMENT,
+    texto VARCHAR(100),
+    dtComent DATE,
+    fkPostagem INT,
+    fkUsuario INT,
+    FOREIGN KEY (fkPostagem) REFERENCES postagem(idpostagem),
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idusuario)
+);
+
+CREATE TABLE seguidores (
+    idseguidores INT PRIMARY KEY AUTO_INCREMENT,
+    dataComecouSeguir DATE,
+    fkUsuario INT,
+    fkUsuarioSeguindo INT,
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idusuario),
+    FOREIGN KEY (fkUsuarioSeguindo) REFERENCES usuario(idusuario)
+);
